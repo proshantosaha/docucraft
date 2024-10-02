6b6876cc9cc5f4968fa4c3e6f1858a0f9070ea5d
proshantosaha <proshantoll71@gmail.com>
2024-10-02 09:59:39 +0600

    layout allDocument data passing props header then passing props sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ docs }) => {
+  const roots = docs.filter((doc) => !doc.parent);
+
+  console.log({ roots });
+
   return (
     <nav className="hidden lg:mt-10 lg:block">
       <ul role="list" className="border-l border-transparent">
@@ -8,7 +12,7 @@ const Sidebar = () => {
           <a
             aria-current="page"
             className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-900 transition dark:text-white"
-            href="/docs"
+            // href="/docs"
           >
             <span className="truncate">Introduction</span>
           </a>
@@ -39,54 +43,6 @@ const Sidebar = () => {
               </a>
             </li>
           </ul>
-        </li>
-        <li className="relative">
-          <a
-            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-            href="/docs/quickstart"
-          >
-            <span className="truncate">Quickstart</span>
-          </a>
-        </li>
-        <li className="relative">
-          <a
-            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-            href="/docs/sdks"
-          >
-            <span className="truncate">SDKs</span>
-          </a>
-        </li>
-        <li className="relative">
-          <a
-            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-            href="/docs/authentication"
-          >
-            <span className="truncate">Authentication</span>
-          </a>
-        </li>
-        <li className="relative">
-          <a
-            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-            href="/docs/pagination"
-          >
-            <span className="truncate">Pagination</span>
-          </a>
-        </li>
-        <li className="relative">
-          <a
-            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-            href="/docs/errors"
-          >
-            <span className="truncate">Errors</span>
-          </a>
-        </li>
-        <li className="relative">
-          <a
-            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-            href="/docs/webhooks"
-          >
-            <span className="truncate">Webhooks</span>
-          </a>
         </li>
       </ul>
     </nav>
